d6ec153fc7b77a471191de95cda6d840bb2d8143
satyamxpandey <satyamxpandey11@gmail.com>
2026-03-22 14:03:09 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+async function generate(){
+
+const prompt = document.getElementById("prompt").value
+
+const res = await fetch("/generate",{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({prompt})
+})
+
+const data = await res.json()
+
+document.getElementById("result").innerHTML=
+`<img src="${data.image}" />`
+
+}
