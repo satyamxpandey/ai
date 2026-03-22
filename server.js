e18399e9c87856f68c696f26cbdd63e12ce4306f
satyamxpandey <satyamxpandey11@gmail.com>
2026-03-22 14:26:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,15 +10,14 @@ app.post("/generate", async (req,res)=>{
 try{
 
 const prompt = req.body.prompt
+const seed = Math.floor(Math.random()*1000000)
 
-const image = `https://image.pollinations.ai/prompt/${encodeURIComponent(prompt)}`
+const image = `https://image.pollinations.ai/prompt/${encodeURIComponent(prompt)}?seed=${seed}&width=1024&height=1024&model=flux`
 
 res.json({image})
 
 }catch(e){
-
 res.json({error:"failed"})
-
 }
 
 })
